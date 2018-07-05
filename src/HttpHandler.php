277b77740ee59c5d7773b016825a086a828ae860
SignpostMarv <me@signpostmarv.name>
2018-07-05 23:24:05 +0100

    php-cs-fixer

--- src/HttpHandler.php
+++ src/HttpHandler.php
@@ -10,10 +10,10 @@ use InvalidArgumentException;
 use SignpostMarv\DaftRouter\DaftSource;
 use SignpostMarv\DaftRouter\Router\Compiler;
 use Symfony\Component\EventDispatcher\EventDispatcher;
-use Symfony\Component\HttpKernel\Event\GetResponseEvent;
-use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
 
 class HttpHandler extends Framework
 {
